998fa3803668b145404509a21006c2b1f681286f
Adam Wistoft <44964494+Wistoft2410@users.noreply.github.com>
2021-04-12 14:06:55 +0200

    Import af gammel IntelliJ kode
    auto-opdatering af workspace.xml samt. uiDesigner.xml

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Board board = new Board(500,500);
+        board.display();
+    }
+}

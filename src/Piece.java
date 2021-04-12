7955fed36db069adca041d87f13ff88b14d78e8d
Adam Wistoft <44964494+Wistoft2410@users.noreply.github.com>
2021-04-12 14:48:29 +0200

    Anvendelse af LinkedList til at fjerne brikker fra boardet

--- src/Piece.java
+++ src/Piece.java
@@ -1,15 +1,34 @@
-public class Piece {
+import java.util.LinkedList;
+
+public class Brik {
     int xp;
     int yp;
     boolean isWhite;
+    LinkedList<Brik> br;
 
-    public Piece(int xp, int yp, boolean isWhite) {
+    public Brik(int xp, int yp, boolean isWhite, LinkedList<Brik> br) {
         this.xp = xp;
         this.yp = yp;
         this.isWhite = isWhite;
+        this.br = br;
+        br.add(this);
     }
 
     public void move(int xp, int yp){
-        
+        /* Tjek om der er en brik på positionen
+           hvis der er en brik kan den dræbes,
+           men hvis der ikke er en kan this.piece rykkes dertil */
+
+        //for-loop der filtrer vores LinkedList gennem og sammenligner piece positionerne
+        for(Brik p : br){
+            if(p.xp == xp && p.yp == yp){
+              p.fjern();
+            }
+        }
+        this.xp=xp;
+        this.yp=yp;
+    }
+    public void fjern(){
+        br.remove(this);
     }
 }

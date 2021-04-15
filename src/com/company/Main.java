e2bdf182996a21d07906bdd7dcded72a5bf99aac
Adam Wistoft <44964494+Wistoft2410@users.noreply.github.com>
2021-04-15 21:34:09 +0200

    En række ændringer:
    En række ændringer:
    
    - Importering af "orginaltBrikBillede"
    - Interval baseret afskåring af "isoleretBrik" i "orginaltBrikBillede"
    -  placering af den valgte "isoleretBrik" sker kun på koordinat (0;0), hvilket er røv ...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.LinkedList;
+
 
 public class Main {
     /*
@@ -23,8 +28,10 @@ public class Main {
 
      */
 
+    //datastrukturen der indeholder brikkerne skal være global og dermed tilgængelig for i alle GameController scopes
+    public static LinkedList<Brik> bb = new LinkedList<>();
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         /*BrikImport brikker = new BrikImport();
         brikker.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         brikker.setVisible(true);
@@ -32,10 +39,27 @@ public class Main {
         brikker.setTitle("Image Program");*/
 
 
-        LinkedList<Brik> br = new LinkedList<>();
+        //orginaltBrikBillede importeres så de enkelte brikker kan blive skåret ud
+        BufferedImage orginaltBrikBillede = ImageIO.read(new File("/Users/computer/Documents/GitHub/Chess-Java-OOP/data/brikker.png"));
+
+        Image brikker[] = new Image[12];
+
+        int isoleretBrik = 0;
+
+        for(int y =0; y < 400; y += 200) {
+            for (int x = 0; x < 1200; x += 200) {
+                brikker[isoleretBrik] = orginaltBrikBillede.getSubimage(x, y, 200, 200).getScaledInstance(64, 64, BufferedImage.SCALE_SMOOTH);
+                isoleretBrik++;
+            }
+        }
+
+        //test brik
+        Brik hkonge = new Brik(,0, true,"konge", bb);
+
         JFrame frame = new JFrame();
         frame.setBounds(10, 10, 512, 512);
         frame.setUndecorated(true);
+
         JPanel board = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
@@ -55,8 +79,42 @@ public class Main {
                     }
                     white = !white;
                 }
+                for (Brik brikBillede: bb) {
+
+                    int isoleretBrik = 0;
+
+                    if (brikBillede.type.equalsIgnoreCase("konge")) {
+                        isoleretBrik = 0;
+                    }
+
+                    if (brikBillede.type.equalsIgnoreCase("dronning")) {
+                        isoleretBrik = 1;
+                    }
+
+                    if (brikBillede.type.equalsIgnoreCase("løber")) {
+                        isoleretBrik = 2;
+                    }
+
+                    if (brikBillede.type.equalsIgnoreCase("springer")) {
+                        isoleretBrik = 3;
+                    }
+
+                    if (brikBillede.type.equalsIgnoreCase("taarn")) {
+                        isoleretBrik = 4;
+                    }
+
+                    if (brikBillede.type.equalsIgnoreCase("bonde")) {
+                        isoleretBrik = 5;
+                    }
+
+                    if (!brikBillede.isWhite) {
+                        isoleretBrik += 6; //rykker 6 frem for at skære de ikke-hvide brikker ud (!brikBillede.isWhite)
+                    }
+                    g.drawImage(brikker[isoleretBrik], brikBillede.xp, brikBillede.yp,this);
+                }
             }
         };
+
         frame.add(board);
         frame.setDefaultCloseOperation(3);
         frame.setVisible(true);

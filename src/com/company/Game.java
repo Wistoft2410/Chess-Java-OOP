0929634d5a118ed16180a2261ea7fa51893de758
Adam Wistoft <44964494+Wistoft2410@users.noreply.github.com>
2021-04-15 23:03:37 +0200

    alle brikker vises ... dog oven i hinanden

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -29,8 +29,45 @@ public class Game {
             }
         }
 
-        //test brik
-        Brik hkonge = new Brik(0,0, true,"konge", bb);
+        //Bag back row
+        Brik brook1 = new Brik(0,0, false,"taarn", bb);
+        Brik bknight1 = new Brik(1,0, false,"springer", bb);
+        Brik bbishop1 = new Brik(2,0, false,"lober", bb);
+        Brik bqueen = new Brik(3,0, false,"dronning", bb);
+        Brik bking = new Brik(4,0, false,"konge", bb);
+        Brik bbishop2 = new Brik(5,0, false,"lober", bb);
+        Brik bknight2 = new Brik(6,0, false,"springer", bb);
+        Brik brook2 = new Brik(7,0, false,"taarn", bb);
+
+        //Black front row
+        Brik bpawn = new Brik(0,1, false,"bonde", bb);
+        Brik bpawn1 = new Brik(1,1, false,"bonde", bb);
+        Brik bpawn2 = new Brik(2,1, false,"bonde", bb);
+        Brik bpawn3 = new Brik(3,1, false,"bonde", bb);
+        Brik bpawn4 = new Brik(4,1, false,"bonde", bb);
+        Brik bpawn5 = new Brik(5,1, false,"bonde", bb);
+        Brik bpawn6 = new Brik(6,1, false,"bonde", bb);
+        Brik bpawn7 = new Brik(7,1, false,"bonde", bb);
+
+        //White back row
+        Brik wrook1 = new Brik(0,7, true,"taarn", bb);
+        Brik wknight1 = new Brik(1,7, true,"springer", bb);
+        Brik wbishop1 = new Brik(2,7, true,"lober", bb);
+        Brik wqueen = new Brik(3,7, true,"dronning", bb);
+        Brik wking = new Brik(4,7, true,"konge", bb);
+        Brik wbishop2 = new Brik(5,7, true,"lober", bb);
+        Brik wknight2 = new Brik(6,7, true,"springer", bb);
+        Brik wrook2 = new Brik(7,7, true,"taarn", bb);
+
+        //White front row
+        Brik hbonde1 = new Brik(0,6, true,"bonde", bb);
+        Brik hbonde2 = new Brik(1,6, true,"bonde", bb);
+        Brik hbonde3 = new Brik(2,6, true,"bonde", bb);
+        Brik hbonde4 = new Brik(3,6, true,"bonde", bb);
+        Brik hbonde5 = new Brik(4,6, true,"bonde", bb);
+        Brik hbonde6 = new Brik(5,6, true,"bonde", bb);
+        Brik hbonde7 = new Brik(6,6, true,"bonde", bb);
+        Brik hbonde8 = new Brik(7,6, true,"bonde", bb);
 
         JFrame frame = new JFrame();
         frame.setBounds(10, 10, 512, 512);
@@ -67,7 +104,7 @@ public class Game {
                         isoleretBrik = 1;
                     }
 
-                    if (brikBillede.type.equalsIgnoreCase("løber")) {
+                    if (brikBillede.type.equalsIgnoreCase("lober")) {
                         isoleretBrik = 2;
                     }
 
